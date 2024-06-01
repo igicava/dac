88bb681149d0feed39875b0c384f8ba963787878
igicava <alexanderurchenco.ru@gmail.com>
2024-06-01 20:42:04 +0300

    modified:   orchestrator/models/models.go

--- orchestrator/models/models.go
+++ orchestrator/models/models.go
@@ -4,7 +4,7 @@ import (
 	"sync"
 )
 
-var Expressions = make(map[string]Expression) // Мапа выражений
+var Expressions = make(map[string]Expression) // Мапа c выражениями
 var Tasks = make(chan Task) // Канал с тасками
 var Mu sync.Mutex // Мьютекс
 
